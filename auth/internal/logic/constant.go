276f3cf03206356558ef608e25326846e6147d7b
luyb177 <2085661244@qq.com>
2025-12-28 21:32:15 +0800

    refactor(auth): 优化用户状态和邀请码状态的常量定义
    Signed-off-by: luyb177 <2085661244@qq.com>

---
+++ auth/internal/logic/constant.go
@@ -0,0 +1,32 @@
+package logic
+
+const (
+	SUPERADMIN = "superadmin"
+	CLASSADMIN = "classadmin"
+	STUDENT    = "student"
+	STAFF      = "staff"
+)
+
+const (
+	InviteCodeActive   = 1
+	InviteCodeInactive = 0
+)
+
+const (
+	NamePrifx = "小安用户"
+)
+
+const (
+	// InvalidUserID 不存在的ID
+	InvalidUserID = iota
+)
+
+const (
+
+	// UserStatusNormal 正常
+	UserStatusNormal = iota + 1
+	// UserStatusDisable 禁用
+	UserStatusDisable
+	// UserStatusDeletion 删除
+	UserStatusDeletion
+)
